e3aeaac354ba7b47ddb7163c7408dafa91911984
jeffreycoho <jeffreycohobupt@gmail.com>
2014-01-04 22:15:51 +0800

    adds v2 cli for network
    Signed-off-by: jeffreycoho <jeffreycohobupt@gmail.com>

---
+++ cli/network_cli_v2.py
@@ -0,0 +1,16 @@
+'''
+Created on Jan 4, 2014
+
+@author: xiejun
+'''
+
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
